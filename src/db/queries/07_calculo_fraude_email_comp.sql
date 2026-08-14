0a0439be191386b9c6e67e41a52860c0581b9663
Lobato2310 <lucaslobsouza@gmail.com>
2026-08-14 14:34:02 -0300

    att email comprador

--- src/db/queries/07_calculo_fraude_email_comp.sql
+++ src/db/queries/07_calculo_fraude_email_comp.sql
@@ -1,5 +1,7 @@
 SELECT 
     categoria_provedor_comprador,
+    COUNT(*) AS total_transacoes,
+    SUM(isFraud) AS total_fraudes,
     ROUND(AVG(isFraud) * 100, 2) AS porcentagem_fraude
 FROM 
     vw_train_clean
